2311843dac0986bdd62babda9c68898590528a8c
Angel Curi Laurente <curilaurente@gmail.com>
2026-03-29 12:36:56 -0500

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "florshop-networking",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "FlorShopNetworking",
+            targets: ["FlorShopNetworking"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "FlorShopNetworking"
+        ),
+        .testTarget(
+            name: "FlorShopNetworkingTests",
+            dependencies: ["FlorShopNetworking"]
+        ),
+    ]
+)
